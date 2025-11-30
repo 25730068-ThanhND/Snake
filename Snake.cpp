b76e4bdd13a7dcf98ecf2f3cac072772ef46bb61
Duong Le Viet Thai <25730066@ms.uit.edu.vn>
2025-11-30 18:15:23 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -68,3 +68,50 @@ void gotoxy( int column, int line )
     coord
     );
   }
+
+void Ve(){
+void Ve(Point Qua){
+    for (int i = 0; i < DoDai; i++){
+        gotoxy(A[i].x, A[i].y);
+        cout<<"X";
+
+    }
+    gotoxy(Qua.x, Qua.y); cout<<"*";
+}
+
+void DiChuyen(int Huong)
+void DiChuyen(int Huong, Point& Qua){
+    for (int i = DoDai-1; i>0; i--)
+        A[i] = A[i-1];
+
+    if (Huong==0) A[0].x = A[0].x + 1;
+    if (Huong==1) A[0].y = A[0].y + 1;
+    if (Huong==2) A[0].x = A[0].x - 1;
+    if (Huong==3) A[0].y = A[0].y - 1;
+
+    if ((A[0].x == Qua.x) && (A[0].y == Qua.y)){
+        DoDai++;
+        Qua.x = rand()%(MAXX-MINX)+MINX;
+        Qua.y = rand()%(MAXY-MINY)+MINY;
+    }
+}
+
+Point Qua;
+srand((int)time(0));
+Qua.x = rand()%(MAXX-MINX)+MINX;
+Qua.y = rand()%(MAXY-MINY)+MINY;
+While  (1){
+    if (kbhit()){
+	t = getch();
+	if (t=='x') Huong = 1;
+    }
+
+
+    system("cls");
+    r.Ve();
+    r.DiChuyen(Huong);
+    r.Ve(Qua);
+    r.DiChuyen(Huong, Qua);
+    Sleep(300);
+}
+
