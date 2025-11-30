a4a35048f32e65dc7c6bac651f23cba5408a1b95
Duong Le Viet Thai <25730066@ms.uit.edu.vn>
2025-11-30 18:15:23 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,82 +1,107 @@
 #include <iostream>
-#include <windows.h>
 #include <cstdlib>
-#include <conio.h>
+#include <ncurses.h>
+#include <unistd.h>   // usleep
 using namespace std;
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 
-void gotoxy( int column, int line );
+void gotoxy(int column, int line);
 
-struct Point{
-    int x,y;
+struct Point {
+    int x, y;
 };
 
-class CONRAN{
+class CONRAN {
 public:
-    struct Point A[100];
+    Point A[100];
     int DoDai;
-    int Diem;     // Thêm biến điểm
 
-    CONRAN(){
+    CONRAN() {
         DoDai = 3;
-        Diem = 0; // Khởi tạo điểm
         A[0].x = 10; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
 
-    void Ve(){
-        for (int i = 0; i < DoDai; i++){
-            gotoxy(A[i].x,A[i].y);
-            cout<<"X";
+    void Ve() {
+        for (int i = 0; i < DoDai; i++) {
+            gotoxy(A[i].x, A[i].y);
+            printw("X");
         }
     }
+    
+    void Ve(Point Qua){
+    for (int i = 0; i < DoDai; i++){
+        gotoxy(A[i].x, A[i].y);
+        cout<<"X";
 
-    void InDiem(){
-        gotoxy(0,0);
-        cout<<"Diem: "<<Diem;
     }
+    gotoxy(Qua.x, Qua.y); cout<<"*";
+}
 
-    void DiChuyen(int Huong){
-        for (int i = DoDai-1; i>0; i--)
-            A[i] = A[i-1];
+    void DiChuyen(int Huong) {
+        for (int i = DoDai - 1; i > 0; i--)
+            A[i] = A[i - 1];
 
-        if (Huong==0) A[0].x = A[0].x + 1;
-        if (Huong==1) A[0].y = A[0].y + 1;
-        if (Huong==2) A[0].x = A[0].x - 1;
-        if (Huong==3) A[0].y = A[0].y - 1;
-
-        Diem++; // Mỗi lần di chuyển +1 điểm
+        if (Huong == 0) A[0].x++;
+        if (Huong == 1) A[0].y++;
+        if (Huong == 2) A[0].x--;
+        if (Huong == 3) A[0].y--;
     }
+    
+    void DiChuyen(int Huong, Point& Qua){
+    for (int i = DoDai-1; i>0; i--)
+        A[i] = A[i-1];
+
+    if (Huong==0) A[0].x = A[0].x + 1;
+    if (Huong==1) A[0].y = A[0].y + 1;
+    if (Huong==2) A[0].x = A[0].x - 1;
+    if (Huong==3) A[0].y = A[0].y - 1;
+
+    if ((A[0].x == Qua.x) && (A[0].y == Qua.y)){
+        DoDai++;
+        Qua.x = rand()%(MAXX-MINX)+MINX;
+        Qua.y = rand()%(MAXY-MINY)+MINY;
+    }
+}
 };
 
-int main()
-{
+int main() {
+    initscr();          // bật ncurses
+    noecho();           // không hiện ký tự nhập
+    curs_set(0);        // ẩn con trỏ
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE); // getch() không chờ
+
     CONRAN r;
     int Huong = 0;
-    char t;
 
-    while (1){
-        if (kbhit()){
-            t = getch();
-            if (t=='a') Huong = 2;
-            if (t=='w') Huong = 3;
-            if (t=='d') Huong = 0;
-            if (t=='x') Huong = 1;
+    while (1) {
+        int t = getch();     // lấy phím không chặn
+
+        if (t != ERR) {
+            if (t == 'a') Huong = 2;
+            if (t == 'w') Huong = 3;
+            if (t == 'd') Huong = 0;
+            if (t == 's' || t == 'x') Huong = 1;
         }
 
-        system("cls");
-        r.InDiem(); // In điểm
+        clear();             // thay cho system("cls")
         r.Ve();
         r.DiChuyen(Huong);
-        Sleep(300);
+
+        refresh();
+        usleep(300000);      // Sleep(300)
     }
 
+    endwin(); // tắt ncurses
     return 0;
 }
 
-void gotoxy( int column, int line ){
-    COORD coord;
-    coord.X = column;
-    coord.Y = line;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+// GOTOXY kiểu Linux
+void gotoxy(int column, int line) {
+    move(line, column);   // (y, x)
 }
