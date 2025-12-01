eea734acef798dbed6201ed70ee7ab0a8cd634ae
xuanphong1408 <hoangphong192837@icloud.com>
2025-12-01 21:02:43 +0700

    Update Snake.cpp
    thêm biến điểm số + hàm in điểm số cho đoạn code , kèm mã hoàn chỉnh đã chỉnh sửa.

--- Snake.cpp
+++ Snake.cpp
@@ -3,34 +3,49 @@
 #include <cstdlib>
 #include <conio.h>
 using namespace std;
+
 void gotoxy( int column, int line );
+
 struct Point{
     int x,y;
 };
+
 class CONRAN{
 public:
     struct Point A[100];
     int DoDai;
+    int Diem;     // Thêm biến điểm
+
     CONRAN(){
         DoDai = 3;
+        Diem = 0; // Khởi tạo điểm
         A[0].x = 10; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
+
     void Ve(){
         for (int i = 0; i < DoDai; i++){
             gotoxy(A[i].x,A[i].y);
             cout<<"X";
         }
     }
+
+    void InDiem(){
+        gotoxy(0,0);
+        cout<<"Diem: "<<Diem;
+    }
+
     void DiChuyen(int Huong){
-        for (int i = DoDai-1; i>0;i--)
+        for (int i = DoDai-1; i>0; i--)
             A[i] = A[i-1];
+
         if (Huong==0) A[0].x = A[0].x + 1;
         if (Huong==1) A[0].y = A[0].y + 1;
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
 
+        Diem++; // Mỗi lần di chuyển +1 điểm
     }
 };
 
@@ -48,7 +63,9 @@ int main()
             if (t=='d') Huong = 0;
             if (t=='x') Huong = 1;
         }
+
         system("cls");
+        r.InDiem(); // In điểm
         r.Ve();
         r.DiChuyen(Huong);
         Sleep(300);
@@ -57,14 +74,9 @@ int main()
     return 0;
 }
 
-
-void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
-    );
-  }
+void gotoxy( int column, int line ){
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
